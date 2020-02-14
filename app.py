2b92410ab8da291026536c7853d59fccfef92fdb
nyebaoscar <nyebaoscar@gmail.com>
2020-02-14 19:10:15 +0300

    Return member after creation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,g
+from flask import Flask, g, request, jsonify
 from database import get_db
 
 app = Flask(__name__)
@@ -17,16 +17,29 @@ def get_member(member_id):
     return 'This returns one member by ID'
 
 @app.route('/member', methods=['POST'])
-def add_number():
-    return 'This adds a new member.'
+def add_member():
+    new_member_data = request.get_json()
+
+    name = new_member_data['name']
+    email = new_member_data['email']
+    level = new_member_data['level']
+
+    db = get_db()
+    db.execute('insert into members (name, email, level) values (?, ?, ?)', [name, email, level])
+    db.commit()
+    
+    member_cur = db.execute('select id, name, email, level from members where name = ?',[name])
+    new_member = member_cur.fetchone()
+
+    return jsonify({'id' : new_member['id'] , 'name' :new_member['name'] , 'email' : new_member['email'], 'level' :new_member['level']})
 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
 def edit_member(member_id):
     return 'This updates a member by ID.'
 
-@app.route('/member/<int:member_id>',methods=['DELETE'])
+@app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
-    return 'This removes a member by ID'
+    return 'This removes a member by ID.'
 
 if __name__ == '__main__':
     app.run(debug=True)
